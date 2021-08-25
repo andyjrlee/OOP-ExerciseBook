abe9c3cc84a9c79c202be988176738d9b6b770c8
andyjrlee <andyjrlee@outlook.com>
2021-08-25 10:18:16 +0100

    AL: first commit and adding hello world example

---
+++ src/main/java/com/natwest/oopexercise/helloworld/OopexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.oopexercise.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopexerciseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/natwest/oopexercise/helloworld/helloworld.java
@@ -0,0 +1,23 @@
+package com.natwest.oopexercise.helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		helloMethod("Andy");
+		
+		System.out.println("Hello World!");
+
+var string = "Hello World!";
+	System.out.println(string);
+	
+	}
+	
+	static void helloMethod(String name) {
+		System.out.println("Hello World, " + name);
+		
+		
+		
+	}
+	
+	
+}
